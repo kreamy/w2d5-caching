4fc92224f5391af2503800eb5a0ac6a5d0b80b40
Karim Habashi <iammonkee@gmail.com>
2017-10-13 18:37:13 -0700

    Worked on hashing and caching

--- LRU Cache/lib/p02_hashing.rb
+++ LRU Cache/lib/p02_hashing.rb
@@ -4,11 +4,46 @@ end
 
 class Array
   def hash
+    # [1,2] --> 8938981
+    # 8938981 --> [1,2]
+    sum = 0
+    # num = 123 if empty?
+
+    nested_count = 0
+    nested_num = 123456789
+
+     if self.empty?
+
+      nested_count += 1
+    end
+    self.each_with_index do |num, idx|
+
+      sum += (num * idx) if num
+      # num = 123456789 if num == []
+      #[]
+      #[[]]
+    end
+    sum
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.each_char.with_index do |char, idx|
+      sum += (char.ord * idx) if char
+    end
+    sum
+  end
+end
+
+class Symbol
+  def hash
+    sum = 0
+    self.to_s.chars.each do |char|
+      sum += char.ord * 2 if char
+    end
+    sum
   end
 end
 
@@ -16,6 +51,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    self.each do |k, v|
+      sum += k.to_s.ord * v.ord if v
+    end
+    sum
   end
 end
--- LRU Cache/lib/p03_hash_set.rb
+++ LRU Cache/lib/p03_hash_set.rb
@@ -9,18 +9,32 @@ class HashSet
   end
 
   def insert(key)
+    num = key.hash
+    unless include?(num)
+      self[num].push(num)
+      @count += 1
+    end
+    resize! if @count > num_buckets
   end
 
   def include?(key)
+    num = key.hash
+    self[num].include?(num)
   end
 
   def remove(key)
+    num = key.hash
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +42,11 @@ class HashSet
   end
 
   def resize!
+    nums_to_dist = @store.flatten
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    nums_to_dist.each do |num|
+      insert(num)
+    end
   end
 end
